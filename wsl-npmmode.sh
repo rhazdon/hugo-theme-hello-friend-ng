f49072eeb36a0609184f511cf5a873ef520bea48
Firehawke <Firehawke@gmail.com>
2020-04-08 19:53:51 -0700

    Add a reminder that WSL-moding npm will unmount /mnt/c

--- wsl-npmmode.sh
+++ wsl-npmmode.sh
@@ -2,3 +2,4 @@
 # this script will turn all of that off.
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 sudo umount /mnt/c
+echo "Remember, you don't have access to /mnt/c so the SSH key passthrough will no longer work on this shell instance."
