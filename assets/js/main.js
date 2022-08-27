a9c3c1599903cdf1321aef646d7021c9bd42ba24
Muzaffar Rayyan Auhammud <16663284+321zer0@users.noreply.github.com>
2022-08-27 21:28:06 +0400

    Fix page reload bug when switching between light and dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,55 @@
 /**
  * Theming.
  *
- * Supports the preferred color scheme of the operation system as well as
+ * Supports the preferred color scheme of the operating system as well as
  * the theme choice of the user.
  *
  */
+
+detectTheme();
+
+function detectTheme() {
+  const themeStorage = "techblog-theme";
+  const chosenTheme = window.localStorage && window.localStorage.getItem(themeStorage);
+  const systemDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  const chosenThemeIsDark = chosenTheme == "dark";
+  const chosenThemeIsLight = chosenTheme == "light";
+
+  if (chosenThemeIsLight) {
+    document.querySelector("#theme-toggle-light").style.setProperty("display", "block");
+    document.querySelector("#theme-toggle-dark").style.setProperty("display", "none");
+    localStorage.setItem(themeStorage, "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  } else if (chosenThemeIsDark || systemDarkMode) {
+    document.querySelector("#theme-toggle-light").style.setProperty("display", "none");
+    document.querySelector("#theme-toggle-dark").style.setProperty("display", "block");
+    localStorage.setItem(themeStorage, "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+}
+
 const themeToggle = document.querySelector(".theme-toggle");
-const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
-const chosenThemeIsDark = chosenTheme == "dark";
-const chosenThemeIsLight = chosenTheme == "light";
 
-// Detect the color scheme the operating system prefers.
-function detectOSColorTheme() {
-  if (chosenThemeIsDark) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else if (chosenThemeIsLight) {
-    document.documentElement.setAttribute("data-theme", "light");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
-
-// Switch the theme.
-function switchTheme(e) {
-  if (chosenThemeIsDark) {
-    localStorage.setItem("theme", "light");
-  } else {
-    localStorage.setItem("theme", "dark");
-  }
-
-  detectOSColorTheme();
-  window.location.reload();
-}
-
-// Event listener
 if (themeToggle) {
   themeToggle.addEventListener("click", switchTheme, false);
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-  window
-    .matchMedia("(prefers-color-scheme: light)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
+}
 
-  detectOSColorTheme();
-} else {
-  localStorage.removeItem("theme");
+function switchTheme(e) {
+  const themeStorage = "techblog-theme";
+  const chosenTheme = window.localStorage && window.localStorage.getItem(themeStorage);
+  const chosenThemeIsDark = chosenTheme == "dark";
+  const chosenThemeIsLight = chosenTheme == "light";
+
+  if (chosenThemeIsDark) {
+    document.querySelector("#theme-toggle-light").style.setProperty("display", "block");
+    document.querySelector("#theme-toggle-dark").style.setProperty("display", "none");
+    localStorage.setItem(themeStorage, "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.querySelector("#theme-toggle-light").style.setProperty("display", "none");
+    document.querySelector("#theme-toggle-dark").style.setProperty("display", "block");
+    localStorage.setItem(themeStorage, "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
 }
