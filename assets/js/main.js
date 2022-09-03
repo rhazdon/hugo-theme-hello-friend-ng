e056636d4cf35b8e1a65f87239080f79d24e12a3
Ivan Dimitrov <ivan@ivandimitrov.co.uk>
2022-09-03 15:36:17 +0000

    fix theme syncs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,35 @@
-/**
- * Theming.
- *
- * Supports the preferred color scheme of the operation system as well as
- * the theme choice of the user.
- *
- */
-const themeToggle = document.querySelector(".theme-toggle");
-const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
-const chosenThemeIsDark = chosenTheme == "dark";
-const chosenThemeIsLight = chosenTheme == "light";
+document.querySelector(".theme-toggle").addEventListener("click", toggleTheme, false);
+initTheme();
 
-// Detect the color scheme the operating system prefers.
-function detectOSColorTheme() {
-  if (chosenThemeIsDark) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else if (chosenThemeIsLight) {
-    document.documentElement.setAttribute("data-theme", "light");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
+function initTheme() {
+	const theme = getLocalStorageOrDeviceTheme();
+	document.documentElement.setAttribute("data-theme", theme);
+	setLocalStorageTheme(theme);
 }
 
-// Switch the theme.
-function switchTheme(e) {
-  if (chosenThemeIsDark) {
-    localStorage.setItem("theme", "light");
-  } else {
-    localStorage.setItem("theme", "dark");
-  }
-
-  detectOSColorTheme();
-  window.location.reload();
+function getDeviceTheme() {
+	return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 }
 
-// Event listener
-if (themeToggle) {
-  themeToggle.addEventListener("click", switchTheme, false);
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-  window
-    .matchMedia("(prefers-color-scheme: light)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-
-  detectOSColorTheme();
-} else {
-  localStorage.removeItem("theme");
+function getLocalStorageTheme() {
+	return window.localStorage.getItem("theme"); 
 }
+
+function setLocalStorageTheme(theme) {
+	window.localStorage.setItem("theme", theme);
+}
+
+function getLocalStorageOrDeviceTheme() {
+	return getLocalStorageTheme() || getDeviceTheme();
+}
+
+function toggleTheme(e) {
+	let newTheme = reverseTheme(getLocalStorageOrDeviceTheme());
+	setLocalStorageTheme(newTheme);
+	window.location.reload();
+}
+
+function reverseTheme(theme) {
+	return theme == "dark" ? "light" : "dark";
+}
+
